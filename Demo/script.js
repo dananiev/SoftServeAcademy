f372b622879522bca30b78c8791eb4c36e4fe86a
Deyan Ananiev <deyan.ananiev@gmail.com>
2021-06-02 20:08:15 +0300

    first commit

--- Demo/script.js
+++ Demo/script.js
@@ -6,6 +6,27 @@ window.addEventListener('scroll', function() {
 
 
 
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.nav li');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+      current = section.getAttribute('id');
+    }
+  })
+
+  navLi.forEach( li => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  })
+})
+
 
 // Read More, Read Less function
 const readMoreBtn = document.querySelector('.readMoreBtn');
@@ -108,7 +129,7 @@ function textFade() {
 
   if (char === splitTextAnimation.length) {
     complete();
-    return;
+    return timer;
   }
 }
 
@@ -121,8 +142,77 @@ function complete() {
 
 // Light/Dark Toggle Button
 const checkbox = document.getElementById('checkbox');
-const aboutPage = document.getElementById('about');
+const aboutPage = document.getElementById('li-about');
 
 checkbox.addEventListener('change', () => {
     aboutPage.classList.toggle('dark');
 })
+
+
+
+
+// Counting Remaining Characters in Textarea Function
+const myTextArea = document.querySelector('.contact-in-textarea');
+const remainingChars = document.querySelector('#count-remaining-characters');
+const maxChars = 250;
+
+myTextArea.addEventListener('input', () => {
+  const remaining = maxChars - myTextArea.value.length;
+  const color = remaining < maxChars * 0.1 ? 'red' : null; 
+
+  remainingChars.textContent = `${remaining} characters remaining`;
+  remainingChars.style.color = color;
+})
+
+function eraseText() {
+  myTextArea.value = '';
+}
+eraseText();
+
+
+
+
+// Academics Flip Functions
+$("#advertising").on("click", () => {
+  $("#advertising").toggleClass("flipping");
+});
+
+$("#entertainment-design").on("click", () => {
+  $("#entertainment-design").toggleClass("flipping");
+});
+
+$("#environmental-design").on("click", () => {
+  $("#environmental-design").toggleClass("flipping");
+});
+
+$("#film").on("click", () => {
+  $("#film").toggleClass("flipping");
+});
+
+$("#fine-art").on("click", () => {
+  $("#fine-art").toggleClass("flipping");
+});
+
+$("#graphic-design").on("click", () => {
+  $("#graphic-design").toggleClass("flipping");
+});
+
+$("#illustration").on("click", () => {
+  $("#illustration").toggleClass("flipping");
+});
+
+$("#interaction-design").on("click", () => {
+  $("#interaction-design").toggleClass("flipping");
+});
+
+$("#photography").on("click", () => {
+  $("#photography").toggleClass("flipping");
+});
+
+$("#product-design").on("click", () => {
+  $("#product-design").toggleClass("flipping");
+});
+
+$("#transportation-design").on("click", () => {
+  $("#transportation-design").toggleClass("flipping");
+});
